79383b13dd5c9ae0a8e7ccb22673d69c8366f6b3
baicha <chengnan0107@163.com>
2022-01-14 18:48:29 +0800

    feat:添加头部和首页基本样式

---
+++ src/components/Topnav.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bc-topnav">
+    <div class="left" @click="onRouterHome">
+      <svg>
+        <use xlink:href="#icon-home"></use>
+      </svg>
+    </div>
+    <div>
+      <a target="_blank" href="https://bc-baicha.github.io/baicha-ui-website/#/"
+        >BC-UI</a
+      >
+      <a target="_blank" href="https://github.com/bc-baicha/bc-blog">Github</a>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { router } from "../router";
+export default {
+  setup() {
+    const onRouterHome = () => {
+      router.push("/");
+    };
+    return {
+      onRouterHome,
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+.bc-topnav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  padding: 0 100px;
+  box-shadow: 0 5px 5px rgba(#333, 0.1);
+  z-index: 10;
+  .left {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      width: 25px;
+      height: 25px;
+    }
+  }
+  a {
+    color: #000;
+    text-decoration: none;
+    font-weight: 500;
+    font-size: 16px;
+    margin-right: 20px;
+  }
+  a:hover {
+    color: #1890ff;
+  }
+}
+</style>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,4 +1,29 @@
 <template>
-  <h1>这是home</h1>
-  <router-link to="/doc">跳转</router-link>
+  <div class="bc-wrap">
+    <Topnav />
+    <main>
+      <img class="bc-home-img" src="../assets/home.gif" alt="" />
+    </main>
+  </div>
 </template>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
+</script>
+<style lang="less" scoped>
+.bc-wrap {
+  width: 100%;
+  height: 100vh;
+  background-color: #d9afd9;
+  background-image: linear-gradient(0deg, #d9afd9 0%, #97d9e1 100%);
+  main {
+    margin-top: 20px;
+    text-align: center;
+    .bc-home-img {
+      width: 700px;
+    }
+  }
+}
+</style>
