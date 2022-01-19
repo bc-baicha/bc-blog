1f62cfa1b49bdf24fba717317b59c2b36d85efd1
baicha <chengnan0107@163.com>
2022-01-19 16:46:50 +0800

    feat:添加中心参数

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,20 @@
   <router-view />
 </template>
 <script lang="ts">
+import { ref, provide } from "vue";
+import { router } from "./router";
 export default {
   name: "App",
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const show = width <= 500 ? false : true;
+    const menuVisible = ref(show);
+    provide("width", menuVisible);
+    router.afterEach(() => {
+      if (width <= 550) {
+        menuVisible.value = false;
+      }
+    });
+  },
 };
 </script>
