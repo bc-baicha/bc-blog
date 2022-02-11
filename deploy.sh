7f26429c015fedfd3e7c7c329a5f39eb7546bf73
baicha <chengnan0107@163.com>
2022-02-11 11:17:28 +0800

    feat:上传发布

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # cd dist && 
 # git init &&
 # git add . &&
-# git commit -m 'first commit' &&
+# git commit -m '上传发布' &&
 # git branch -M main &&
 # git remote add origin git@github.com:bc-baicha/bc-blog-website.git &&
 # git push -f -u origin main &&
