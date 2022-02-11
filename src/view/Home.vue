1965c7918143f1618a3339b4a82be32182234a84
baicha <chengnan0107@163.com>
2022-02-11 11:24:48 +0800

    fix:修改进入的判断条件

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -5,15 +5,26 @@
       <img class="bc-home-img" src="../assets/home.gif" alt="" />
       <Playtext text="今天也是元气满满的一天~~" :width="370" />
       <div class="text"><a>简单的记录点东西</a></div>
-      <router-link to="/doc" class="btn-grad">Get Started -></router-link>
+      <div @click="onPush" class="btn-grad">Get Started -></div>
     </main>
   </div>
 </template>
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
 import Playtext from "../components/Playtext.vue";
+import { router } from "../router";
 export default {
   components: { Topnav, Playtext },
+  setup() {
+    const onPush = () => {
+      const showKey = localStorage.getItem("showKey");
+      if (!showKey) {
+        localStorage.setItem("showKey", "JavaScript");
+      }
+      router.replace("/doc");
+    };
+    return { onPush };
+  },
 };
 </script>
 <style lang="less" scoped>
@@ -79,6 +90,7 @@ export default {
       margin: 0 auto;
       margin-top: 40px;
       font-size: 20px;
+      cursor: pointer;
       background-image: linear-gradient(
         to right,
         #314755 0%,
