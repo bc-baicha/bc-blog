87c2b51d8584b68038e06fbbf95952d82fb725dc
baicha <chengnan0107@163.com>
2022-02-23 17:33:08 +0800

    fix:修改table的样式

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,32 +1,32 @@
 <template>
-  <table>
+  <table class="tableVue">
     <tbody>
       <slot />
     </tbody>
   </table>
 </template>
 <style lang="less">
-table {
+.tableVue {
   border-collapse: collapse;
   background-color: #ebebeb;
-}
-table,
-td,
-th {
-  border: 1px solid #ccc;
-}
-th {
-  min-width: 300px;
-  padding: 15px 5px;
-  white-space: pre-wrap;
-  text-align: center;
-  vertical-align: middle;
-}
-td {
-  padding: 15px 15px;
-  width: 500px;
-  white-space: pre-wrap;
-  text-align: left;
-  vertical-align: middle;
+  table,
+  td,
+  th {
+    border: 1px solid #ccc;
+  }
+  th {
+    min-width: 300px;
+    padding: 15px 5px;
+    white-space: pre-wrap;
+    text-align: center;
+    vertical-align: middle;
+  }
+  td {
+    padding: 15px 15px;
+    width: 500px;
+    white-space: pre-wrap;
+    text-align: left;
+    vertical-align: middle;
+  }
 }
 </style>
