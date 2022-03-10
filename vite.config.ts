bd18534a786676ff3ce867fceebeace9fdf61cdd
baicha <chengnan0107@163.com>
2022-03-10 18:59:56 +0800

    fix:配置路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: './',
   plugins: [
     vue()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
