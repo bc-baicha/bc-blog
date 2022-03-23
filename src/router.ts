e151aea0cbffb1e33d1b04ae5405c001cf173fa4
baicha <chengnan0107@163.com>
2022-03-23 18:53:15 +0800

    fix:修改路由引入方法

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,30 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from './view/Home.vue';
 import Docpage from './view/Docpage.vue';
+/**这是js模块 */
+import letConst from './lib/JS/letConst.vue';
+import Parameter from './lib/JS/Parameter.vue';
+import ClassArray from './lib/JS/ClassArray.vue';
+import Closure from './lib/JS/Closure.vue';
+import Prototype from './lib/JS/Prototype.vue';
+import Packagetool from './lib/JS/Packagetool.vue';
+import Newintroduce from './lib/JS/Newintroduce.vue';
+import FCexporess from './lib/JS/FCexpress.vue';
+import Stackheap from './lib/JS/Stackheap.vue';
+import Moduleloaded from './lib/JS/Moduleloaded.vue';
+/** 这是CSS模块*/
+import Multipleborders from './lib/CSS/Multipleborders.vue';
+import Stripesbackground from './lib/CSS/Stripesbackground.vue';
+import Ellipse from './lib/CSS/Ellipse.vue';
+import Poptip from './lib/CSS/Poptip.vue';
+import Interview from './lib/CSS/Interview.vue';
+/**这是React模块 */
+import React from './lib/React/index.vue';
+import Introduce from './lib/React/React-hooks/introduce.vue';
+import Reactredux from './lib/React/React-redux/index.vue';
+import Eventprocess from './lib/React/Event/Eventprocess.vue';
+import Syntheticevent from './lib/React/Event/Syntheticevent.vue';
+import Lifecycle from './lib/React/Lifecycle.vue'
 const history = createWebHashHistory();
 const pathFC = (value) => {
   return import(`./lib/${value}.vue`)
@@ -14,34 +38,34 @@ const router = createRouter({
       redirect: '/doc/letConst',
       children: [
         // 这是js模块
-        { path: 'letConst', component: pathFC('JS/LetConst') },
-        { path: 'parameter', component: pathFC('JS/Parameter') },
-        { path: 'classArray', component: pathFC('JS/ClassArray') },
-        { path: 'closure', component: pathFC('JS/Closure') },
-        { path: 'prototype', component: pathFC('JS/Prototype') },
-        { path: 'packagetool', component: pathFC('JS/Packagetool') },
-        { path: 'newintroduce', component: pathFC('JS/Newintroduce') },
-        { path: 'fcexpress', component: pathFC('JS/FCexpress') },
-        { path: 'stackHeap', component: pathFC('JS/Stackheap') },
-        { path: 'moduleLoaded', component: pathFC('JS/Moduleloaded') },
+        { path: 'letConst', component: letConst },
+        { path: 'parameter', component: Parameter },
+        { path: 'classArray', component: ClassArray },
+        { path: 'closure', component: Closure },
+        { path: 'prototype', component: Prototype },
+        { path: 'packagetool', component: Packagetool },
+        { path: 'newintroduce', component: Newintroduce },
+        { path: 'fcexporess', component: FCexporess },
+        { path: 'stackHeap', component: Stackheap },
+        { path: 'moduleLoaded', component: Moduleloaded },
         // 这是CSS模块
-        { path: 'multipleborders', component: pathFC('CSS/Multipleborders') },
-        { path: 'stripesbackground', component: pathFC('CSS/Stripesbackground') },
-        { path: 'ellipse', component: pathFC('CSS/Ellipse') },
-        { path: 'poptip', component: pathFC('CSS/Poptip') },
-        { path: 'interview', component: pathFC('CSS/Interview') },
-        { path: 'gridstudy', component: pathFC('CSS/Gridstudy') },
+        { path: 'multipleborders', component: Multipleborders },
+        { path: 'stripesbackground', component: Stripesbackground },
+        { path: 'ellipse', component: Ellipse },
+        { path: 'poptip', component: Poptip },
+        { path: 'interview', component: Interview },
         {
-          path: 'react', component: import('./lib/React/index.vue'),
+          path: 'react', component: React,
           redirect: '/doc/react/introduce',
           children: [
-            { path: 'introduce', component: pathFC('React/React-hooks/Introduce') },
-            { path: 'redux', component: import('./lib/React/React-redux/index.vue') },
-            { path: 'eventprocess', component: import('./lib/React/Event/Eventprocess.vue') },
-            { path: 'syntheticevent', component: import('./lib/React/Event/Syntheticevent.vue') },
-            { path: 'lifecycle', component: pathFC('React/Lifecycle') }
+            { path: 'introduce', component: Introduce },
+            { path: 'redux', component: Reactredux },
+            { path: 'eventprocess', component: Eventprocess },
+            { path: 'syntheticevent', component: Syntheticevent },
+            { path: 'lifecycle', component: Lifecycle }
           ]
         }
+
       ]
     }
   ]
