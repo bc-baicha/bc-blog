9c2b1de6415c2092548c5c11fab86364b210ea75
baicha <chengnan0107@163.com>
2023-06-21 18:39:28 +0800

    fix:注释变量

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-rm -rf dist &&
-pnpm run build &&
-cd dist && 
-git init &&
-git add . &&
-git commit -m '上传发布' &&
-git branch -M main &&
-git remote add origin git@github.com:bc-baicha/bc-blog-website.git &&
-git push -f -u origin main &&
-cd -
-echo https://bc-baicha.github.io/bc-blog-website/
+# rm -rf dist &&
+# pnpm run build &&
+# cd dist && 
+# git init &&
+# git add . &&
+# git commit -m '上传发布' &&
+# git branch -M main &&
+# git remote add origin git@github.com:bc-baicha/bc-blog-website.git &&
+# git push -f -u origin main &&
+# cd -
+# echo https://bc-baicha.github.io/bc-blog-website/
