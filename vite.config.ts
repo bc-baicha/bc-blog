fdb4d115ed04a2c8de18f337658d7bfe745161d0
zhangshuai <zhangshuai@mita.com>
2023-10-27 10:53:38 +0800

    fix：添加配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // import { fileURLToPath, URL } from 'url'
-import vue from '@vitejs/plugin-vue';
+import vue from "@vitejs/plugin-vue";
 export default defineConfig({
-  base: './',
-  plugins: [
-    vue()
-  ],
+  base: "./",
+  plugins: [vue()],
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
     alias: {
-      '@': '/src'
-    }
+      "@": "/src",
+    },
   },
-})
+  server: {
+    port: 3003,
+  },
+});
